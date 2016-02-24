af0463e2f281a79ccf4f49d54b82186a0dab0d94
Reupen Shah <reupen@users.noreply.github.com>
2016-02-24 21:06:33 +0000

    initial support for paginated lists

--- aiogithub/base.py
+++ aiogithub/base.py
@@ -1,7 +1,12 @@
 import asyncio
-import aiohttp
 
+import aiohttp
+import link_header
+
+from .base_object import BaseList
 from .user import User
+
+MAX_ITEMS = 200
 
 
 class GitHub:
@@ -11,16 +16,29 @@ class GitHub:
         self.timeout = 10
         self.base_url = 'https://api.github.com'
 
-    async def get_url(self, path):
+    async def get_absolute_url(self, url):
         with aiohttp.Timeout(self.timeout):
-            async with self.client.get(self.base_url + '/' + path) as response:
-                print(response.status)
-                print(response.headers)
+            async with self.client.get(url) as response:
                 limits = {
                     'limit': response.headers.get('X-RateLimit-Limit'),
                     'remaining': response.headers.get('X-RateLimit-Limit')
                 }
-                return User(await response.json(), limits)
+                link_header_value = response.headers.get('link')
+                links_dict = None
+                if link_header_value:
+                    links = link_header.parse(link_header_value)
+                    links_dict = {link.rel: link.href for link in links.links}
+                return await response.json(), limits, links_dict
+
+    async def get_url(self, path):
+        return await self.get_absolute_url(self.base_url + '/' + path)
+
+    async def get_user(self, user_name):
+        return User(*await self.get_url('user/' + user_name))
+
+    async def get_users(self, since=None, max_items=MAX_ITEMS):
+        return BaseList(self, User, *await self.get_url('users'),
+                        max_items=max_items)
 
     def close(self):
         self.client.close()
--- aiogithub/base_object.py
+++ aiogithub/base_object.py
@@ -1,4 +1,5 @@
-from collections import UserDict
+from collections import UserDict, abc
+
 import dateutil.parser
 
 
@@ -16,11 +17,50 @@ class BaseObject(UserDict):
 
 
 class BaseResponseObject(BaseObject):
-    def __init__(self, document, limits):
+    def __init__(self, document, limits, links=None):
         super().__init__(document)
 
         self._limits = BaseObject(limits)
+        self._links = links
 
     @property
     def limits(self):
         return self._limits
+
+
+class BaseList(abc.AsyncIterable):
+    def __init__(self, client, element_type, initial_document, limits, links,
+                 max_items=None):
+        self._client = client
+        self._element_type = element_type
+        self._current_list = initial_document
+        self._current_iter = None
+        self._limits = BaseObject(limits)
+        self._last_raw_limits = limits
+        self._max_items = max_items
+        self._links = links
+        self._item_counter = len(initial_document)
+
+    @property
+    def limits(self):
+        return self._limits
+
+    async def __aiter__(self):
+        self._current_iter = iter(self._current_list)
+        return self
+
+    async def __anext__(self):
+        try:
+            value = next(self._current_iter)
+        except StopIteration:
+            if self._item_counter < self._max_items and 'next' in self._links:
+                document, limits, links = await self._client.get_absolute_url(
+                    self._links['next'])
+                self._current_list = document
+                self._last_raw_limits = limits
+                self._links = links
+                self._item_counter += len(document)
+                self._current_iter = iter(self._current_list)
+                return await self.__anext__()
+            raise StopAsyncIteration
+        return self._element_type(value, self._last_raw_limits)
