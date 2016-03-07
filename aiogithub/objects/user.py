ee73c0d72c8c0ea3ee89de0622d3fd2be1c79a5e
Reupen Shah <reupen@users.noreply.github.com>
2016-03-07 21:52:14 +0000

    add user properties

--- aiogithub/objects/user.py
+++ aiogithub/objects/user.py
@@ -1,6 +1,8 @@
-from aiogithub.objects.base_object import BaseResponseObject
+from datetime import datetime
 
+from aiogithub.objects.base_object import BaseResponseObject
 from aiogithub import objects
+from aiogithub.utils import return_key
 
 
 class User(BaseResponseObject):
@@ -38,3 +40,93 @@ class User(BaseResponseObject):
     async def get_received_events(self):
         return await self._get_related_url('received_events_url',
                                            objects.Event)
+
+    @property
+    @return_key
+    def login(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def id(self) -> int:
+        pass
+
+    @property
+    @return_key
+    def gravatar_id(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def html_url(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def type(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def site_admin(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def company(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def blog(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def location(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def email(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def hireable(self) -> bool:
+        pass
+
+    @property
+    @return_key
+    def bio(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def public_repos(self) -> int:
+        pass
+
+    @property
+    @return_key
+    def public_gists(self) -> int:
+        pass
+
+    @property
+    @return_key
+    def followers(self) -> int:
+        pass
+
+    @property
+    @return_key
+    def following(self) -> int:
+        pass
+
+    @property
+    @return_key
+    def created_at(self) -> datetime:
+        pass
+
+    @property
+    @return_key
+    def updated_at(self) -> datetime:
+        pass
