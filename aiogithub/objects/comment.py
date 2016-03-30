904130b43dfb7e8df085e2dc388a754ceef6c1e0
Reupen Shah <reupen@users.noreply.github.com>
2016-03-30 20:24:29 +0100

    add comment properties

--- aiogithub/objects/comment.py
+++ aiogithub/objects/comment.py
@@ -1,5 +1,45 @@
+from datetime import datetime
+
+from aiogithub import objects
 from aiogithub.objects.base_object import BaseResponseObject
+from aiogithub.utils import return_key
 
 
 class Comment(BaseResponseObject):
-    pass
+    _url = 'repos/{user}/{repo}/issues/comments/{id}'
+
+    @staticmethod
+    def _get_key_mappings():
+        return {
+            'user': objects.User
+        }
+
+    @property
+    @return_key
+    def id(self) -> int:
+        pass
+
+    @property
+    @return_key
+    def html_url(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def body(self) -> str:
+        pass
+
+    @property
+    @return_key
+    def user(self) -> 'objects.User':
+        pass
+
+    @property
+    @return_key
+    def created_at(self) -> datetime:
+        pass
+
+    @property
+    @return_key
+    def updated_at(self) -> datetime:
+        pass
