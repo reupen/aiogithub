634e5b67698c4f567f08bebd155b33642a7b4426
Reupen Shah <reupen@users.noreply.github.com>
2016-09-21 07:07:04 +0100

    setup.py improvements

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+import sys
 
 from setuptools import setup, find_packages
+
+setup_requires = []
+is_test = {'pytest', 'test'} & set(sys.argv)
+
+if is_test:
+    setup_requires=[
+        'pytest-runner~=2.9',
+        'pytest~=3.0'
+    ]
+
 
 setup(
     name="aiogithub",
@@ -11,8 +22,8 @@ setup(
 
     include_package_data=True,
 
-    setup_requires=['pytest-runner', 'pytest'],
-    tests_require=['pytest-asyncio'],
+    setup_requires=setup_requires,
+    tests_require=['pytest-asyncio~=0.5.0'],
 
     author="Reupen Shah",
     description="asyncio-based GitHub API client",
