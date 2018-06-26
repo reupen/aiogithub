0b7dad7733dbea89ae310d448e6e0565a57be047
Philipp A <flying-sheep@web.de>
2018-06-26 10:26:21 +0200

    Exclude tests explicitly

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     use_scm_version={
         'write_to': os.path.join('aiogithub', 'version.py')
     },
-    packages=['aiogithub'],
+    packages=find_packages(exclude=['tests*']),
     install_requires=[
         'aiohttp~=3.0',
         'async-timeout',
